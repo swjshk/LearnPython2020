4c7364ee8823c525eba36d9480898a1d2848395b
Wenjun Sang <wenjunsang@Wenjuns-iMac.local>
2020-05-17 23:45:18 -0400

    add all files

--- PythonBasics/StringFormatting.py
+++ PythonBasics/StringFormatting.py
@@ -1,7 +1,7 @@
-'''
+"""
 format you output
 2 ways
-'''
+"""
 
 # Method 1
 # formatted string literals 'f' or 'F'
@@ -17,14 +17,32 @@ yes_votes = 42_572_654
 # print(yes_votes)
 no_votes = 43_132_495
 percentage = yes_votes / (yes_votes + no_votes)
-str2 = '{:-9} Yes votes  {:2.2%}'.format(yes_votes,percentage) 
-#use string slicing and concatenation to format the passed string
-#print(str2)
+str2 = "{:-9} Yes votes  {:2.2%}".format(yes_votes, percentage)
+# use string slicing and concatenation to format the passed string
+# print(str2)
 
 import math
 
 # print(f"The value of pi is {math.pi:.3f}.")
 
-table = {"Sjoerd": 4127, "Jack": 4098, "Dcab": 8637678}
 
-print("Jack: {Jack:D}; Sjoerd: {Sjoerd:d}; Dcab: {Dacb:d}".format(**table))
+
+print('{0} and {1}'.format('spam', "eggs"))
+print('{1} and {0}'.format('spam', "eggs"))
+# {0} position of the objects passed into the .format()
+
+print('This {food} is {adjective}.'.format(
+    food="spam", adjective='absolutely horrible'))
+# keyword arguments
+
+table = {"Sjoerd": 4127, "Jack": 4098, "Dcab": 8637678}
+print("Jack: {Jack:d}; Sjoerd: {Sjoerd:d}; Dcab: {Dcab:d}".format(**table))
+# pass the tabel as keyword args with '**'
+print('jack: {0[Jack]:d}; Sjoerd: {0[Sjoerd]:d}; Dcab: {0[Dcab]:d}'.format(table))
+
+# format specification mini-language
+for x in range(1, 11):
+    print('{0:2d} {1:3d} {2:4d}'.format(x, x*x,x*x*x))
+
+
+test=1
--- ex6.py
+++ ex6.py
@@ -1,27 +1,27 @@
 import datetime
 
-string1="this is {} {}"
-A="A"
-B="B"
-print(string1.format(A,B))
-count=0
+string1 = "this is {} {}"
+A = "A"
+B = "B"
+print(string1.format(A, B))
+count = 0
 
 while input("enter 1"):
-    formatter="Ack by {} @{} SESA{}"
-    time=datetime.datetime.now()
-    date_time=time.strftime("%H:%M:%S")
-    if count==0:
-        OE="Max"
-        SESA="242524"
-        count=1
+    formatter = "Ack by {} @{} SESA{}"
+    time = datetime.datetime.now()
+    date_time = time.strftime("%H:%M:%S")
+    if count == 0:
+        OE = "Max"
+        SESA = "242524"
+        count = 1
     else:
-        OE="Brian"
-        SESA="123445"
-        count=0
-    print(formatter.format(OE,date_time,SESA))
+        OE = "Brian"
+        SESA = "123445"
+        count = 0
+    print(formatter.format(OE, date_time, SESA))
 
-print('end of code')
+print("end of code")
 
-formatter2=f"todays date is{date_time}"
+formatter2 = f"todays date is{date_time}"
 print(formatter2)
 print()
